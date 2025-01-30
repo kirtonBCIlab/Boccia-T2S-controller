796118a4de38e248e7fa214f42a59aec5f0b3ca1
daniellarbourque <daniellarosebourque@gmail.com>
2025-01-30 15:45:38 -0700

    Disable drop key for 15 seconds

--- boccia-gui/key_press_handler.py
+++ boccia-gui/key_press_handler.py
@@ -1,5 +1,6 @@
 # Default libraries
 from PyQt5.QtCore import QObject
+from PyQt5.QtCore import QTimer, Qt
 
 # Custom libraries
 from commands import Commands
@@ -11,6 +12,10 @@ class KeyPressHandler(QObject):
         self.serial_handler = serial_handler
 
         self.key_pressed = None # Store the key pressed for hold commands
+
+        self.key_enabled = True # True if the key is enabled
+
+        self.timer = None # Timer for key lock
 
 
     def eventFilter(self, obj, event):
@@ -35,9 +40,40 @@ class KeyPressHandler(QObject):
                 self.key_pressed = key
 
             elif key in Commands.TOGGLE_COMMANDS:
-                print(f"User key pressed: {key}")
-                command = Commands.TOGGLE_COMMANDS[key]
-                self.serial_handler.send_command(command)
+                # If the drop key is pressed, start a timer to lock it
+                if key == Qt.Key_3:
+
+                    print(f"User key pressed: {key}")
+
+                    # If the key is already disabled, return
+                    if self.key_enabled == False:
+                        # print("Key is disabled")
+                        return
+                    
+                    # If it is enabled, send the command then disable it
+                    command = Commands.TOGGLE_COMMANDS[key]
+                    self.serial_handler.send_command(command)
+                    print("Sent command")
+
+                    # Disable the key
+                    self.key_enabled = False
+
+                    # Stop timer if it exists
+                    if self.timer:
+                        self.timer.stop()
+
+                    # Start the timer
+                    self.timer = QTimer(self)
+                    self.timer.setSingleShot(True)
+                    self.timer.timeout.connect(lambda: self._reenable_key())
+                    self.timer.start(15000) # Locked for 15 seconds
+                    print("Key disabled for 15 seconds")
+
+                else:
+                    # Send the command normally if a different key is pressed
+                    print(f"User key pressed: {key}")
+                    command = Commands.TOGGLE_COMMANDS[key]
+                    self.serial_handler.send_command(command)
                 
             event.accept()
 
@@ -52,3 +88,8 @@ class KeyPressHandler(QObject):
                 self.key_pressed = None
 
             event.accept()
+
+
+    def _reenable_key(self):
+        # Re-enable key
+        self.key_enabled = True
