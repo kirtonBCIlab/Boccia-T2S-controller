80210ecd939924f3085ca3dd379a5ce741e598dd
daniellarbourque <daniellarosebourque@gmail.com>
2025-05-14 16:13:35 -0600

    Fix error again

--- boccia-gui/test_bluetooth_server.py
+++ boccia-gui/test_bluetooth_server.py
@@ -16,7 +16,7 @@ class BluetoothServer(QObject):
         self.service_info.setServiceName("Bluetooth Test Server")
         self.service_info.setServiceDescription("Testing Bluetooth server")
         self.service_info.setServiceProvider("PyQt5")
-        self.service_info.setDevice(self.local_device.address())
+        # self.service_info.setDevice(self.local_device.address())
         self.service_info.setServiceUuid(QBluetoothUuid(QBluetoothUuid.SerialPort))
         
         self.bluetooth_server.listen(self.local_device.address())
