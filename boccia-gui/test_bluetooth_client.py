a46910663400bee5970a5f3b511a36d7183c727e
daniellarbourque <daniellarosebourque@gmail.com>
2025-05-14 15:25:48 -0600

    Checkpoint

--- boccia-gui/test_bluetooth_client.py
+++ boccia-gui/test_bluetooth_client.py
@@ -1,16 +1,17 @@
 from PyQt5.QtCore import QObject, QTimer, QCoreApplication
-from PyQt5.QtBluetooth import QBluetoothSocket, QBluetoothUuid
+from PyQt5.QtBluetooth import QBluetoothSocket, QBluetoothUuid, QBluetoothServiceInfo, QBluetoothAddress
 from PyQt5.QtWidgets import QApplication
 
 class BluetoothClient(QObject):
     def __init__(self, address):
         super().__init__()
-        self.socket = QBluetoothSocket(QBluetoothSocket.RfcommSocket)
+        self.socket = QBluetoothSocket(QBluetoothServiceInfo.RfcommProtocol)
         self.socket.readyRead.connect(self.on_data_received)
         self.socket.connected.connect(self.on_connected)
-        self.socket.errorOccurred.connect(lambda error: print(f"Socket error: {error}"))
+        self.socket.error.connect(lambda error: print(f"Socket error: {error}"))
 
-        self.socket.connectToService(address, QBluetoothUuid.SerialPort)
+        bluetooth_address = QBluetoothAddress(address)
+        self.socket.connectToService(bluetooth_address, QBluetoothUuid.SerialPort)
         print("Connecting to Bluetooth device...")
 
     def on_connected(self):
@@ -24,7 +25,7 @@ class BluetoothClient(QObject):
 
 if __name__ == "__main__":
     app = QCoreApplication([])
-    server_address = "00:11:22:33:44:55"
+    server_address = "E8:9C:25:5D:AA:42"
     client = BluetoothClient(server_address)
 
     QTimer.singleShot(10000, app.quit)  # Auto-quit after 10 seconds for testing purposes
