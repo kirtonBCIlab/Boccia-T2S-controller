dbbd3bd6e73912e8bfb09caa09cc8fec47feb7f5
daniellarbourque <daniellarosebourque@gmail.com>
2025-06-13 11:03:25 -0600

    Update instructions

--- boccia-gui/multiplayer_instructions_widget.py
+++ boccia-gui/multiplayer_instructions_widget.py
@@ -25,19 +25,20 @@ class MultiplayerInstructionsWidget(QWidget):
         self.main_layout.addLayout(self.instructions_section_layout)
 
     def _create_instructions_section(self):
-        self.instructions_title = QLabel("Follow these steps to setup Bluetooth connection for Multiplayer Mode:")
+        self.instructions_title = QLabel("Follow these steps to setup the devices for Multiplayer Mode:")
         self.instructions_title.setStyleSheet(Styles.INSTRUCTIONS_TEXT)
 
         instructions_text = (
             "1. Pair the devices in Bluetooth settings.<br>"
-            "2. Make sure Bluetooth remains turned on for both devices.<br>"
+            "2. Make sure Bluetooth remains turned on for all devices.<br>"
             "3. In the Main Device app:<br>"
             "&nbsp;&nbsp;&nbsp;a) Click 'Turn ON Multiplayer Mode'.<br>"
             "&nbsp;&nbsp;&nbsp;b) Select the number of players in the dropdown menu.<br>"
             "&nbsp;&nbsp;&nbsp;c) Click 'Connect'.<br>"
             "&nbsp;&nbsp;&nbsp;d) Wait until the status says 'Waiting for other devices'.<br>"
             "4. In the Multiplayer Device app (this app):<br>"
-            "&nbsp;&nbsp;&nbsp;a) Click the dropdown menu to see the list of paired devices.<br>"
+            "&nbsp;&nbsp;&nbsp;a) Click the dropdown menu to see the list of paired Bluetooth devices. "
+            "Please wait a few seconds for the list to update.<br>"
             "&nbsp;&nbsp;&nbsp;b) Select the name of the Main Device.<br>"
             "&nbsp;&nbsp;&nbsp;c) Click 'Connect'."
         )
