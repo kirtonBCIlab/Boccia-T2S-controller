46f51b7be293ebb187bfe93059f232893140e338
Daniella Bourque <daniella.bourque@ucalgary.ca>
2026-05-21 11:02:13 -0600

    Update main.py

--- boccia-gui/main.py
+++ boccia-gui/main.py
@@ -4,7 +4,7 @@ from main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
-    mainWindow = MainWindow(include_multiplayer_controls=True)
+    mainWindow = MainWindow(include_multiplayer_controls=False)
     mainWindow.show()
     sys.exit(app.exec_())
 
